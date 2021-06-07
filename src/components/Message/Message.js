9c519e62692f54c8ef346c60a1b664b9935fed7c
saaketh <saakethaj@gmail.com>
2021-06-07 17:03:03 +0530

    message date, time and username done

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,9 +1,21 @@
 import React from "react";
 import "./Message.css";
 
-function Message({ children, right, uname, time }) {
+function Message({ children, right, uname = "Unknown user", time = "00:00" }) {
   return (
-    <div className={`Message ${right && "Right__Message"}`}>{children}</div>
+    <div className={`Message ${right && "Right__Message"}`}>
+      {!right && (
+        <>
+          <div className="Message__uname">{uname}</div>
+        </>
+      )}
+      {children}
+      <>
+        <div className={`Message__time ${right && "Message__time__right"}`}>
+          {time}
+        </div>
+      </>
+    </div>
   );
 }
 
